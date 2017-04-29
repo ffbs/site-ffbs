18c21372a7d00923503ba3a24fabe5639be735da
Chrissi^ <chris@tinyhost.de>
2017-04-29 14:21:55 +0200

    Build Name construction from master

--- site.mk
+++ site.mk
@@ -26,7 +26,7 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := $(shell bash -c 'while [[ `pwd` != *"/gluon" ]]; do cd ..; done; site/getRelease.sh')-$(shell date '+%Y%m%d')-beta
+DEFAULT_GLUON_RELEASE := $(shell bash -c 'while [[ -z `git remote -v  | grep "freifunk-gluon/gluon.git"` ]]; do cd ..; done; site/getRelease.sh')-$(shell date '+%Y%m%d')-beta
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
